591502bfd1c480b0e90c33d9918b3a14b54acabb
graberzz <mrneshta@gmail.com>
2017-08-14 00:08:33 +0500

    adaptive, fixes

--- js/script.js
+++ js/script.js
@@ -4,4 +4,20 @@ var slider = tns({
     slideBy: 'page',
     autoplay: false,
     controls: false,
+    responsive: {
+      '0': 1,
+      '540': 2,
+      '700': 3,
+      '910': 4,
+    },
+
   });
+
+var menuToggle = document.getElementsByClassName('main-nav__toggle')[0];
+
+menuToggle.addEventListener('click', function(){
+  var menu = document.getElementsByClassName('main-nav__list')[0];
+  var toggleLine = document.getElementsByClassName('main-nav__toggle-line')[0];
+  menu.classList.toggle('main-nav__list--active');
+  toggleLine.classList.toggle('main-nav__toggle-line--active');
+});
